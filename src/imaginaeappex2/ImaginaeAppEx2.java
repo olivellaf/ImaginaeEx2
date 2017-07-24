381012573404c7b38b8c865c40c5156e16d8897d
Ferran Olivella <ferry87@gmail.com>
2017-07-24 21:26:47 +0200

    Minor changes 2

--- src/imaginaeappex2/ImaginaeAppEx2.java
+++ src/imaginaeappex2/ImaginaeAppEx2.java
@@ -19,8 +19,8 @@ public class ImaginaeAppEx2 {
         int[] starter_arr = new int[1000];
         int[] second_arr = new int[100];
         
-        int[] pares_arr = new int[100];
-        int[] impares_arr = new int[100];
+        int[] pares_arr = new int[50];
+        int[] impares_arr = new int[50];
         
         int[] third_arr = new int[100];
         
@@ -32,7 +32,7 @@ public class ImaginaeAppEx2 {
         
         // De este array, debemos obtener los 100 últimos y asignarlos a otro Array nuevo pero en orden inverso.
         int j=0;
-        for (int i = starter_arr.length-1; i >= starter_arr.length-100-1; i--) {
+        for (int i = starter_arr.length-1; i >= starter_arr.length-100; i--) {
             if (j < second_arr.length) {
                 second_arr[j] = starter_arr[i];
 //                System.out.println(second_arr[j] + " j: " + j);
@@ -42,7 +42,7 @@ public class ImaginaeAppEx2 {
         
         // Del nuevo array, extraer los impares, añadirlos a un nuevo array y hacer lo mismo con los pares.
         int m = 0; int p = 0;
-        for (int i = 0; i < second_arr.length-1; i++) {
+        for (int i = 0; i < second_arr.length; i++) {
             if (second_arr[i]%2 == 0) {
                 // número par
                 pares_arr[p] = second_arr[i];
@@ -64,10 +64,11 @@ public class ImaginaeAppEx2 {
          * sólo se añadirán aquellos que al restar 100 al resultado no sea 
          * menor que 0.
          */
-        for (int i = 0; i < pares_arr.length-1; i++) {
+        
+        int t = 0;
+        for (int i = 0; i < pares_arr.length; i++) {
             int multiplicacion = pares_arr[i]*impares_arr[i];
             int resta = multiplicacion - 100;
-            int t = 0;
             
             if (resta >= 0) {
                 third_arr[t] = resta;
